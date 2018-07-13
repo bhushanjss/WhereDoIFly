6747dfa3d1c734863818c53380d22772904f07ab
Vidya Singh <bhushanjss@gmail.com>
2018-07-13 15:28:20 -0400

    input style fix

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -24,20 +24,18 @@ const styles = {
     color: '#000',
     paddingRight: 5,
     paddingLeft: 5,
+    paddingTop: 5,
     fontSize: 18,
-    lineHeight: 23,
-    flex: 2
+    lineHeight: 23
   },
   labelStyle: {
-    fontSize: 18,
-    paddingLeft: 20,
-    flex: 1
+    fontSize: 8,
+    paddingLeft: 5
   },
   containerStyle: {
     height: 40,
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center'
+    flexDirection: 'column'
   }
 };
 
