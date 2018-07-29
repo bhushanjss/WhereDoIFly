6417e648ea0bac39da067626acaa3120d0e98115
Vidya Singh <bhushanjss@gmail.com>
2018-07-29 03:01:24 -0400

    added react native element and add button.

--- src/components/ShowFlights.js
+++ src/components/ShowFlights.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { Button } from 'react-native-elements';
 import { connect } from 'react-redux';
-import { FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import FlightItem from './FlightItem';
 
 class ShowFlights extends Component {
@@ -10,11 +11,27 @@ class ShowFlights extends Component {
 
   render() {
     return (
+      <View>
       <FlatList
       data={this.props.flights}
       renderItem={this.renderItem}
       keyExtractor={flight => flight.id}
       />
+      <Button
+        title='Add'
+        buttonStyle={{
+        width: 100,
+        height: 45,
+        position: 'absolute',
+        bottom: 20,
+        right: 10,
+        borderColor: 'transparent',
+        borderWidth: 0,
+        borderRadius: 5
+        }}
+        containerStyle={{ marginTop: 20 }}
+      />
+      </View>
     );
   }
 }
